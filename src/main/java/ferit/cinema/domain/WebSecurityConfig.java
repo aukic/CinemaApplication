4d1dd1773b59955886458947b9f7fc32942d43ed
aukic <anteukic@hotmail.com>
2023-01-06 22:18:02 +0100

    Locked spring-security-config version to 5.6.7 and edited WebSecurityConfig implementation

--- src/main/java/ferit/cinema/domain/WebSecurityConfig.java
+++ src/main/java/ferit/cinema/domain/WebSecurityConfig.java
@@ -1,25 +1,25 @@
-package ferit.cinema.security;
+package ferit.cinema.domain;
 
 import ferit.cinema.feature.user.UserService;
 import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @AllArgsConstructor
 @EnableWebSecurity
-public class WebSecurityConfig {
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     private final UserService userService;
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
         http
                 .csrf().disable()
                 .authorizeRequests()
@@ -28,11 +28,10 @@ public class WebSecurityConfig {
                 .anyRequest()
                 .authenticated().and()
                 .formLogin();
-        return http.build();
     }
 
-    @Autowired
-    void registerProvider(AuthenticationManagerBuilder auth){
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.authenticationProvider(daoAuthenticationProvider());
     }
 
